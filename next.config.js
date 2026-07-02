4c63da1d3d7a5115832e1192ba861e9be59338cf
radhakantarout <radhakanta.rout16@gmail.com>
2026-07-02 16:08:27 +1000

    fix: use bundled /public gallery photos as demo instead of picsum.photos

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
       { protocol: 'https', hostname: 'previews.vayustudios.com' },
-      { protocol: 'https', hostname: 'picsum.photos' },
       ...(process.env.CLOUDFRONT_DOMAIN
         ? [{ protocol: 'https', hostname: process.env.CLOUDFRONT_DOMAIN.replace('https://', '') }]
         : []),
